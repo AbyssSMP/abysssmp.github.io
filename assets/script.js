206e079b8f01d0f51a2409c79a9b58ce9ef83510
TakkunMC_jp <Kaneda.family.803@gmail.com>
2025-12-17 20:30:53 +0900

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const themeToggle = document.getElementById("themeToggle");
+themeToggle.addEventListener("click", () => {
+  const current = document.documentElement.getAttribute("data-theme") || "dark";
+  const next = current === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", next);
+  localStorage.setItem("theme", next);
+});
